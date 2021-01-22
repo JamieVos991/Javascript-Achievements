4c3e61da0231bfa88747d464ff30e0abc501187b
Jamie <31694@ma-web.nl>
2021-01-22 11:33:19 +0100

    Edits

---
+++ JSB2L5 Quiz/quiz1.js
@@ -0,0 +1,73 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+        "title": "Quiz lol",
+        "imageURI": "https://i.ytimg.com/vi/QqjnlQ55Jnw/maxresdefault.jpg"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "Hoe oud ben ik?",
+				"answers": [{
+						"answer": 15,
+						"feedback": false
+					},
+					{
+						"answer": "16",
+						"feedback": true
+					},
+					{
+						"answer": "17",
+						"feedback": false
+					},
+					{
+						"answer": "18",
+						"feedback": false
+					},
+				]
+			},
+			{
+				"question": "Wie is gwn boef man?",
+				"answers": [{
+						"answer": "Ewa",
+						"feedback": false
+					},
+					{
+						"answer": "Boef",
+						"feedback": false
+					},
+					{
+						"answer": "Bang Bang",
+						"feedback": true
+					},
+					{
+						"answer": "?",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Welk nummer is niet van de beatles?",
+				"answers": [{
+						"answer": "Let it be",
+						"feedback": false
+					},
+					{
+						"answer": "Come together",
+						"feedback": false
+					},
+					{
+						"answer": "Here comes the moon",
+						"feedback": true
+					},
+					{
+						"answer": "Yellow submarine",
+						"feedback": false
+					}
+				]
+			}
+		]
+	}
---
+++ JSB2L5 Quiz/quiz2.js
@@ -0,0 +1,73 @@
+const quiz2 = 
+{
+    "quizID": "Math 1",
+    "quizMetaData":
+    {
+        "title": "Math Trivia  1",
+        "imageURI": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/x3y3z3k-1568041112.jpg?crop=0.987xw:0.790xh;0.00160xw,0.128xh&resize=980:*"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "Hoe oud ben ik?",
+				"answers": [{
+						"answer": "15",
+						"feedback": false
+					},
+					{
+						"answer": "16",
+						"feedback": false
+					},
+					{
+						"answer": "17",
+						"feedback": true
+					},
+					{
+						"answer": "18s",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Wie is gwn boef man?",
+				"answers": [{
+						"answer": "12",
+						"feedback": false
+					},
+					{
+						"answer": "22",
+						"feedback": false
+					},
+					{
+						"answer": "32",
+						"feedback": true
+					},
+					{
+						"answer": "",
+						"feedback" : false
+					}
+				]
+			},
+			{
+				"question": "Welk nummer is niet van de beatles?",
+				"answers": [{
+						"answer": "Let it be",
+						"feedback": false
+					},
+					{
+						"answer": "Come together",
+						"feedback": false
+					},
+					{
+						"answer": "Here comes the moon",
+						"feedback": true
+					},
+					{
+						"answer": "Yellow submarine",
+						"feedback": false
+					}
+				]
+			}
+		]
+	}
